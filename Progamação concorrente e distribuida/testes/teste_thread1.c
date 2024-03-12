6bec3483c3647b6c3ec3cfafe01f711194ac7d7f
André Filipe Siqueira Tokumoto <145510795+andretokumoto@users.noreply.github.com>
2024-03-12 20:45:06 -0300

    pcd

---
+++ Progamação concorrente e distribuida/testes/teste_thread1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+#include <string.h>
+
+
+int main(int argc, char **argv) {
+    thread_t tp[NP];
+
+
+    return 0;
+}
