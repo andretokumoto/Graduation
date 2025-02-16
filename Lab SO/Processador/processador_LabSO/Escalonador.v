715069013f84f0acd69494da606aa1f0364d3eb5
André Filipe Siqueira Tokumoto <145510795+andretokumoto@users.noreply.github.com>
2025-02-16 19:22:01 -0300

    escalonador com todas as funções - versão 1

--- Lab SO/Processador/processador_LabSO/Escalonador.v
+++ Lab SO/Processador/processador_LabSO/Escalonador.v
@@ -1,16 +1,71 @@
 module Escalonador(
     input clock,
     input reset,
-    input pc_atual,
-	 output reg processo_atual,
+	 input [31:0] novo_processo,
+	 input controle_novo_processo,
+    output reg [31:0] processo_atual, // Número do processo atual
+    output reg troca_contexto // Sinal de troca de contexto
 );
 
-	reg contador = 32'd0;
-	reg [31:0] processos [4:0]; //lista de processos rodando
-	
-	parameter quantum = 4'd20;
-	
-	
-	
+    
+    parameter quantum = 32'd20; 
+
+    reg [31:0] processos [0:9]; // Lista de processos 
+    reg [3:0] indice_processo_atual = 4'd0; //processo atual
+    reg [31:0] contador = 32'd0; 
+	 reg [3:0] num_processos;
+
+
+	 always@(posedge controle_novo_processo)
+		begin
+			
+			processos[num_processos] = novo_processo;
+			num_processos = num_processos + 4'd1;
+			
+		end
+	 
+  
+    always @(posedge clock or posedge reset) begin
+			
+			//resetando escalonador
+        if (reset) begin
+            contador <= 32'd0;
+            indice_processo_atual <= 4'd0;
+            processo_atual <= 32'd0; 
+            troca_contexto <= 1'b0; 
+				num_processos <= 4'd0;
+        end
+        else begin
+            
+            contador <= contador + 32'd1;
+
+            // quantum foi atingido
+            if (contador >= quantum) begin
+       
+                contador <= 32'd0;
+
+                // Alterna processo
+                if (indice_processo_atual < num_processos - 1) 
+						begin
+                    indice_processo_atual <= indice_processo_atual + 4'd1;
+						end
+						
+                else 
+						begin
+                    indice_processo_atual <= 4'd0; // Volta para o primeiro processo
+						end
+
+                // atualiza processo atual
+                processo_atual <= processos[indice_processo_atual];
+
+                // sinal de troca de contexto
+                troca_contexto <= 1'b1;
+            end
+            else begin
+                // troca de contexto desativado
+                troca_contexto <= 1'b0;
+            end
+        end
+    end
 
 endmodule
