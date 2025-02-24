5941ec1822060c2b3b29f2ae6df9cee228aff1fa
andretokumoto <andretokumoto@gmail.com>
2025-02-24 15:53:01 -0300

    registrador para processo atual

--- Lab SO/Processador/processador_LabSO/correcaoDesvio.v
+++ Lab SO/Processador/processador_LabSO/correcaoDesvio.v
@@ -6,11 +6,11 @@ module correcaoDesvio(
 
 );
 	TAM_BLOCO = 32'd200;
-	reg [31:0] valorCorrecao;
+	reg [10:0] valorCorrecao;
 
 	always@(processo_atual)
 		begin
-			valorCorrecao = TAM_BLOCO*processo_atual;//calcula novo valor de pc de desvio
+			valorCorrecao = TAM_BLOCO*processo_atual[10:0];//calcula novo valor de pc de desvio
 		end
 
 	always@(desvioOriginal)
