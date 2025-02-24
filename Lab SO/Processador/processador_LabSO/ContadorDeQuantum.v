8b522eb6feb8f489fd042f753623f28d49a91cd5
andretokumoto <andretokumoto@gmail.com>
2025-02-24 08:38:10 -0300

    passar so para mem intruções

--- Lab SO/Processador/processador_LabSO/ContadorDeQuantum.v
+++ Lab SO/Processador/processador_LabSO/ContadorDeQuantum.v
@@ -5,7 +5,8 @@ module ContadorDeQuantum(
   input InstrucaIO,
   input fimProcesso,
   output reg troca_contexto // Sinal de troca de contexto
-  output reg [31:0] pc_processo_trocado,
+  output reg [31:0] pc_processo_trocado,//salva o pc do processo interrompido
+  output reg intrucaoIOContexto//sinal para dar salto em intrução de io
 );
 
  parameter quantum = 32'd10; 
@@ -17,22 +18,30 @@ module ContadorDeQuantum(
 		if(reset || fimProcesso)begin
 			contador=32'd0;
 			troca_contexto = 1'b0;
+			intrucaoIOContexto = 1'b0;
 		end
 		
 		
 		else
 			begin
 				
-					if(contador>=quantum || InstrucaIO) //quantum atingido ou intruçao de entrada/saida
+					if(contador>=quantum) //quantum atingido 
 						begin
 							pc_processo_trocado = pc+32'd1; //salva pc do processo atual + 1
 							troca_contexto = 1'b1;//da sinal para o sistema realizar a troca de contexto
 							contador = 32'd0;//zera o contador
 						end
+
+					else if(InstrucaIO)//intruçao de entrada/saida
+						begin
+							pc_processo_trocado = pc+32'd1; //salva pc do processo atual + 1
+							intrucaoIOContexto = 1'b1;
+						end
 					
 					else
 						begin
 							troca_contexto = 1'b0;//para pc atualizar normalmente
+							intrucaoIOContexto = 1'b0;
 							contador = contador + 32'd1;
 						end
 					
