a764282d3bdc7a995ef34160d4c5d006328de508
André Filipe Siqueira Tokumoto <145510795+andretokumoto@users.noreply.github.com>
2025-07-19 14:18:36 -0300

    logica de envio de comando e acoes para recebimentlo
    esta com problema para mandar as mensagens para o lcd

--- Sistemas Embarcados/Projeto final/master.c
+++ Sistemas Embarcados/Projeto final/master.c
@@ -1,49 +1,94 @@
-// --- Ligacoes entre PIC e LCD ---
-sbit LCD_RS at RE2_bit;   // PINO 2 DO PORTD INTERLIGADO AO RS DO DISPLAY
-sbit LCD_EN at RE1_bit;   // PINO 3 DO PORTD INTERLIGADO AO EN DO DISPLAY
-sbit LCD_D7 at RD7_bit;  // PINO 7 DO PORTD INTERLIGADO AO D7 DO DISPLAY
-sbit LCD_D6 at RD6_bit;  // PINO 6 DO PORTD INTERLIGADO AO D6 DO DISPLAY
-sbit LCD_D5 at RD5_bit;  // PINO 5 DO PORTD INTERLIGADO AO D5 DO DISPLAY
-sbit LCD_D4 at RD4_bit;  // PINO 4 DO PORTD INTERLIGADO AO D4 DO DISPLAY
+#include <string.h>  // Necessário para strcmp
 
-// Selecionando direcao de fluxo de dados dos pinos utilizados para a comunicacao com display LCD
-sbit LCD_RS_Direction at TRISE2_bit;  // SETA DIRECAO DO FLUXO DE DADOS DO PINO 2 DO PORTD
-sbit LCD_EN_Direction at TRISE1_bit;  // SETA DIRECAO DO FLUXO DE DADOS DO PINO 3 DO PORTD
-sbit LCD_D7_Direction at TRISD7_bit;  // SETA DIRECAO DO FLUXO DE DADOS DO PINO 7 DO PORTD
-sbit LCD_D6_Direction at TRISD6_bit;  // SETA DIRECAO DO FLUXO DE DADOS DO PINO 6 DO PORTD
-sbit LCD_D5_Direction at TRISD5_bit;  // SETA DIRECAO DO FLUXO DE DADOS DO PINO 5 DO PORTD
-sbit LCD_D4_Direction at TRISD4_bit;  // SETA DIRECAO DO FLUXO DE DADOS DO PINO 4 DO PORTD
+// --- Ligações entre PIC e LCD ---
+sbit LCD_RS at RE2_bit;   // PINO 2 DO PORTE INTERLIGADO AO RS DO DISPLAY
+sbit LCD_EN at RE1_bit;   // PINO 3 DO PORTE INTERLIGADO AO EN DO DISPLAY
+sbit LCD_D7 at RD7_bit;   // PINO 7 DO PORTD INTERLIGADO AO D7 DO DISPLAY
+sbit LCD_D6 at RD6_bit;   // PINO 6 DO PORTD INTERLIGADO AO D6 DO DISPLAY
+sbit LCD_D5 at RD5_bit;   // PINO 5 DO PORTD INTERLIGADO AO D5 DO DISPLAY
+sbit LCD_D4 at RD4_bit;   // PINO 4 DO PORTD INTERLIGADO AO D4 DO DISPLAY
 
-char ucRead;        // Variavel para armazenar o dado lido.
+// Direção dos pinos usados para comunicação com o LCD
+sbit LCD_RS_Direction at TRISE2_bit;
+sbit LCD_EN_Direction at TRISE1_bit;
+sbit LCD_D7_Direction at TRISD7_bit;
+sbit LCD_D6_Direction at TRISD6_bit;
+sbit LCD_D5_Direction at TRISD5_bit;
+sbit LCD_D4_Direction at TRISD4_bit;
 
-void main(){
+char recebido;        // Variável para armazenar o dado lido
+char comando[50];     // Buffer para a string
+unsigned int i = 0;
+unsigned char ucStatus = 1;
 
-   ADCON1  = 0x0E;                           //Configura os pinos do PORTB como digitais, e RA0 (PORTA) como analogico
+void main() {
+    ADCON1  = 0x0E;                 // Configura RA0 como analógico, o resto como digital
+    TRISB.RB1 = 1;                  // Botão de entrada
 
-   Lcd_Init();                               //Inicializa m dulo LCD
-   Lcd_Cmd(_LCD_CURSOR_OFF);                 //Apaga cursor
-   Lcd_Cmd(_LCD_CLEAR);                      //Limpa display
+    Delay_ms(50);                  // Pequeno atraso inicial
 
-   UART1_Init(9600);  // Utiliza bibliotecas do compilador para configurcao do Baud rate.
+    Lcd_Init();                    // Inicializa LCD
+    Lcd_Cmd(_LCD_CURSOR_OFF);      // Oculta cursor
+    Lcd_Cmd(_LCD_CLEAR);           // Limpa display
 
+    UART1_Init(9600);              // Inicializa UART
 
-   while(1){
+    while (1) {
+        // Controle do botão para acionar alarme
+        if ((PORTB.RB1 == 0) && (ucStatus == 0)) {
+            ucStatus = 1;
+            UART1_Write('d');  // Envia comando para disparar alarme
+            Delay_ms(200);     // Debounce
+        } else if ((PORTB.RB1 == 0) && (ucStatus == 1)) {
+            ucStatus = 0;
+            Delay_ms(200);     // Debounce
+        }
 
+        // Recepção de comandos via UART
+        if (UART1_Data_Ready()) {
+            char c = UART1_Read();
 
-       if(UART1_Data_Ready()){  // Verifica se um dado foi recebido do arduino
-       
-         Delay_ms(50);   // Pausa de 50ms.
-         
-         if (ucRead == 'd'){  //alarme foi disparado
+            if (c == '\r' || c == '\n') {
+                comando[i] = '\0';  // Finaliza string
 
-            //Ativar o buzer
-            //ativar leds do alarme - efeito fade
-            lcd_out(1,1,"ALARME DISPARADO!!");
-           
-          }
-          else if(ucRead == 'p'){
-                lcd_out(1,1,"Presenca detectada");
-                //leds de atençao
-          }
-       }
+                if (strcmp(comando, "desativar") == 0) {
+                    UART1_Write(0);
+                } else if (strcmp(comando, "ativar") == 0) {
+                    UART1_Write(1);
+                }
+
+                i = 0;  // Reseta índice
+            } else {
+                if (i < sizeof(comando) - 1) {
+                    comando[i++] = c;
+                }
+            }
+        }
+
+        // Resposta a comandos recebidos do Arduino
+        if (UART1_Data_Ready()) {
+            recebido = UART1_Read();  // Corrigido: agora lê o dado
+            Delay_ms(50);
+
+            if (recebido == 'd') {
+            
+                Lcd_Cmd(_LCD_CLEAR);
+                Delay_ms(10);
+                Lcd_Out(1,1,"ALARME DISPARADO!!");
+            } 
+            else if (recebido == 'p') {
+            
+                Lcd_Cmd(_LCD_CLEAR);
+                Delay_ms(10);
+                Lcd_Out(1,1,"Presenca detectada");
+            } 
+            else if (recebido == 'a') {
+            
+                Lcd_Cmd(_LCD_CLEAR);
+                Delay_ms(10);
+                Lcd_Out(1,1,"ALARME DISPARADO!!");
+                Lcd_Out(2,1,"PORTA ABERTA!!");
+            }
+        }
+    }
 }
