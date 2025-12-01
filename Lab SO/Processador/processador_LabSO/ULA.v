88f066781104cf5e8c21192da0c03bf9a6230c01
André Filipe Siqueira Tokumoto <145510795+andretokumoto@users.noreply.github.com>
2025-11-30 21:53:47 -0300

    alteração na ula para calcular apenas na borda do clock

--- Lab SO/Processador/processador_LabSO/ULA.v
+++ Lab SO/Processador/processador_LabSO/ULA.v
@@ -1,4 +1,4 @@
-module ULA(ulaOP,RS,RT,saidaULA,saidaHI,saidaLO);
+module ULA(clock,ulaOP,RS,RT,saidaULA,saidaHI,saidaLO);
 
 input [31:0] RS,RT;
 input  [4:0]ulaOP;
@@ -8,7 +8,7 @@ output reg[31:0] saidaULA,saidaHI,saidaLO;
 parameter soma=5'b00000,subtracao=5'b00001,multiplicacao=5'b00010,divisao=5'b00011,restoDivisao=5'b00100,OPor=5'b00101,OPand=5'b00110,OPnot=5'b00111;
 parameter OPxor=5'b01000,OPnor=5'b01001,OPnand=5'b01010,OPxnor=5'b01011,maior=5'b01110, seguidor = 5'b11111;
 
-always@(*)
+always@(posedge clock)
 begin
   case(ulaOP)
 
