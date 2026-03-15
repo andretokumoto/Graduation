e9e6e7fa37e47167da8e9615f9c4dda4baeadd44
André Filipe Siqueira Tokumoto <145510795+andretokumoto@users.noreply.github.com>
2026-03-15 20:42:14 -0300

    sinais de controle para analise de IO
    preparação para verificar sinais durante processo de interrupção por IO

--- Lab SO/Processador/processador_LabSO/CPU.v
+++ Lab SO/Processador/processador_LabSO/CPU.v
@@ -14,8 +14,8 @@ module CPU(
     output wire [6:0] uniProc,
 	 
 	 //***********************testes***********************
-	 output wire testeSinal
-	 /*output reg [31:0] testePC,
+	 output wire testeSinal,
+	 output reg [31:0] testePC,
     output wire [4:0] enRD,
     output wire [4:0] enRS,
     output wire [4:0] enRT,
@@ -41,7 +41,7 @@ module CPU(
 	 output wire testeMemControl,
 	 output wire teste_troca_contexto,
 	 output wire teste_sinal_cproc,
-	 output wire teste_fim*/
+	 output wire teste_fim
 );
 
     // Declarações internas
@@ -143,7 +143,7 @@ module CPU(
     
     assign halt = parada;
 	 assign un = rs[3:0];
-	/* assign testeSelecaoMuxDesvio = selecaoMuxDesvio;
+	assign testeSelecaoMuxDesvio = selecaoMuxDesvio;
 	 assign testeBranchControl = branchControl;
     assign testeResultComparacao = resultComparacao;
     assign testeIN = dadosDeEntrada;
@@ -167,7 +167,7 @@ module CPU(
 	 assign teste_troca_contexto = troca_contexto;
 	 assign teste_sinal_cproc = mudaProcesso;
 	 assign teste_fim = fimprocesso;
-	 //assign Testeprocesso_atual = processo_rodando;*/
+	 //assign Testeprocesso_atual = processo_rodando;
     
     always@(negedge clk) 
     begin
@@ -195,7 +195,7 @@ module CPU(
                 else pc <= resulSomador;
             end    
         end
-       // testePC <= pc;
+       testePC <= pc;
     end
     
     always@(pc)
