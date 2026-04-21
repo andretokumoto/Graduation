5fe7d705f64633648b82981a7a348ba47dbc34fe
André Filipe Siqueira Tokumoto <145510795+andretokumoto@users.noreply.github.com>
2026-04-21 14:11:16 -0300

    modulo de teste no arduino

--- Lab Comunicacao Digital/LabComunicacao_Arduino/LabComunicacao_Arduino.ino
+++ Lab Comunicacao Digital/LabComunicacao_Arduino/LabComunicacao_Arduino.ino
@@ -1,18 +1,30 @@
+#include <SoftwareSerial.h>
+
+// Define pinos: RX no 10, TX no 11
+// Conecte: TX da FPGA -> Pino 10 (RX) do Arduino
+// Conecte: Pino 11 (TX) do Arduino -> Divisor de Tensão -> RX da FPGA
+SoftwareSerial fpgaserial(10, 11); 
+
 void setup() {
-
-  Serial.begin(9600);
-
+  Serial.begin(9600);      // Comunicação com o PC (Monitor Serial)
+  fpgaserial.begin(9600);  // Comunicação com a FPGA
+  
+  Serial.println("Sistema Pronto. Digite um numero para enviar a FPGA:");
 }
 
 void loop() {
-
+  // 1. Recebe do PC e manda para a FPGA
   if (Serial.available()) {
-
     char c = Serial.read();
-
-    Serial.print("Recebido: ");
+    fpgaserial.write(c); 
+    Serial.print("Enviado para FPGA: ");
     Serial.println(c);
-
   }
 
+  // 2. Recebe da FPGA e manda para o PC
+  if (fpgaserial.available()) {
+    char c = fpgaserial.read();
+    Serial.print("Recebido da FPGA: ");
+    Serial.println(c);
+  }
 }
